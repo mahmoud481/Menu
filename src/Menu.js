28922bfe8acc3e9da057e13c7550802cd80f6832
mahmoud481 <mah.mohamed48@gmail.com>
2021-08-26 02:42:31 +0200

    show menu based on category

---
+++ src/Menu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Menu = ({items}) => {
+  return (
+    <div className="section-center">
+      {
+        items.map((menuItem)=>{
+          const {id,title,category,img,desc,price} = menuItem;
+          return (
+            <article key={id} className="menu-item">
+                <img className="photo" src={img} alt={desc}/>
+                <div className="item-info">
+                  <header>
+                    <h4>{title}</h4>
+                    <h4 className="price">{price}</h4>
+                  </header>
+                  <p className="item-text">{desc}</p>
+                </div>
+            </article>
+          )
+        })
+      }
+    </div>
+  )
+};
+
+export default Menu;
